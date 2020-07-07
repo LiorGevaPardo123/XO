a5cafb1e606847667bbf9e41eeca29543d661a06
LiorGevaPardo123 <67971153+LiorGevaPardo123@users.noreply.github.com>
2020-07-07 19:42:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonPartie = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(21)))), ((int)(((byte)(34)))));
+            this.panel1.Controls.Add(this.buttonPartie);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(57, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(393, 92);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonPartie
+            // 
+            this.buttonPartie.BackColor = System.Drawing.Color.White;
+            this.buttonPartie.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.buttonPartie.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.buttonPartie.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonPartie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPartie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.buttonPartie.Location = new System.Drawing.Point(214, 21);
+            this.buttonPartie.Name = "buttonPartie";
+            this.buttonPartie.Size = new System.Drawing.Size(154, 53);
+            this.buttonPartie.TabIndex = 9;
+            this.buttonPartie.Text = "New Game";
+            this.buttonPartie.UseVisualStyleBackColor = false;
+            this.buttonPartie.Click += new System.EventHandler(this.ButtonPartie_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(15, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Play Now";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(130)))), ((int)(((byte)(21)))), ((int)(((byte)(34)))));
+            this.panel2.Controls.Add(this.button9);
+            this.panel2.Controls.Add(this.button8);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(41, 110);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(440, 440);
+            this.panel2.TabIndex = 1;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button9.Location = new System.Drawing.Point(295, 295);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(140, 140);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button8.Location = new System.Drawing.Point(149, 295);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(140, 140);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button7.Location = new System.Drawing.Point(3, 295);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(140, 140);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button6.Location = new System.Drawing.Point(295, 149);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(140, 140);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button5.Location = new System.Drawing.Point(149, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 140);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button4.Location = new System.Drawing.Point(3, 149);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 140);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button3.Location = new System.Drawing.Point(295, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 140);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button2.Location = new System.Drawing.Point(149, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 140);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 140);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tic_Tac_Toe.Properties.Resources.giphy;
+            this.pictureBox1.Location = new System.Drawing.Point(487, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 440);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(790, 567);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonPartie;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Tic_Tac_Toe
+{  
+    
+    //SoundPlayer sO = new SoundPlayer();
+    
+    public partial class Form1 : Form
+    { 
+        int XorO = 0;
+        bool S = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SoundPlayer s = new SoundPlayer(@Tic_Tac_Toe.Properties.Resources.Open);
+            s.Play();
+            S = true;
+            XorO = 0;
+            win = false;
+            label1.Text = "Play Now";
+            foreach (Control c in panel2.Controls)
+            {
+                if (c is Button)
+                {
+                    c.Text = "";
+                    c.BackColor = Color.White;
+                }
+            }
+            // add action to all buttons inside panel2
+            foreach (Control c in panel2.Controls)
+            {
+                if (c is Button)
+                {
+                    c.Click += new System.EventHandler(btn_click);
+                }
+            }
+        }        
+        // create button 
+        public void btn_click(object sender, EventArgs e)
+        {
+            if (win!=true)
+            {
+                SoundPlayer sR = new SoundPlayer(@Tic_Tac_Toe.Properties.Resources.רוני_פוני);
+                sR.Play();
+            }
+            Button btn = (Button)sender;
+            if (win==false&&btn.Text == "")
+            {
+                if (XorO % 2 == 0)
+                {
+                    btn.Text = "X";
+                    btn.ForeColor = Color.Blue;
+                    label1.Text = "O turn now";
+                    getTheWinner();
+                }
+                else
+                {
+                    btn.Text = "O";
+                    btn.ForeColor = Color.Red;
+                    label1.Text = "X turn now";
+                    getTheWinner();
+                }
+                XorO++;
+            }
+        }
+        bool win = false;
+        // get the winner 
+        public void getTheWinner()
+        {
+            if (!button1.Text.Equals("") && button1.Text.Equals(button2.Text) && button1.Text.Equals(button3.Text))
+            {
+                winEffect(button1, button2, button3);
+                win = true;
+            }
+            if (!button4.Text.Equals("") && button4.Text.Equals(button5.Text) && button4.Text.Equals(button6.Text))
+            {
+                winEffect(button4, button5, button6);
+                win = true;
+            }
+            if (!button7.Text.Equals("") && button7.Text.Equals(button8.Text) && button7.Text.Equals(button9.Text))
+            {
+                winEffect(button7, button8, button9);
+                win = true;
+            }
+            if (!button1.Text.Equals("") && button1.Text.Equals(button4.Text) && button1.Text.Equals(button7.Text))
+            {
+                winEffect(button1, button4, button7);
+                win = true;
+            }
+            if (!button2.Text.Equals("") && button2.Text.Equals(button5.Text) && button2.Text.Equals(button8.Text))
+            {
+                winEffect(button2, button5, button8);
+                win = true;
+            }
+            if (!button3.Text.Equals("") && button3.Text.Equals(button6.Text) && button3.Text.Equals(button9.Text))
+            {
+                winEffect(button3, button6, button9);
+                win = true;
+            }
+            if (!button1.Text.Equals("") && button1.Text.Equals(button5.Text) && button1.Text.Equals(button9.Text))
+            {
+                winEffect(button1, button5, button9);
+                win = true;
+            }
+            if (!button3.Text.Equals("") && button3.Text.Equals(button5.Text) && button3.Text.Equals(button7.Text))
+            {
+                winEffect(button3, button5, button7);
+                win = true;
+            }
+
+            // if no one win
+            // if all buttons are not empty 
+            // we can but 1 char in a button "X or O"
+            // we have 9 buttons 
+            // mean 9 char in length
+            if (AllBtnLength() == 9 && win == false)
+            {
+                SoundPlayer sH = new SoundPlayer(Tic_Tac_Toe.Properties.Resources.תיקו);
+                sH.Play();
+                label1.Text = "No Winner";
+            }
+
+        }
+        // get all button text length    
+        public int AllBtnLength()
+        {
+            int allTextButtonsLength = 0;
+            foreach (Control c in panel2.Controls)
+            {
+                if (c is Button)
+                {
+                    allTextButtonsLength += c.Text.Length;
+                }
+            }
+            return allTextButtonsLength;
+        }              
+        public void winEffect(Button b1, Button b2, Button b3)
+        {
+            if (b1.Text=="O")
+            {
+                SoundPlayer sX = new SoundPlayer(Tic_Tac_Toe.Properties.Resources.X);
+                sX.Play();
+            }
+            else
+            {
+                SoundPlayer sO = new SoundPlayer(Tic_Tac_Toe.Properties.Resources.O);
+                sO.Play();
+            }
+
+            b1.BackColor = Color.FromArgb(130, 21, 34);
+            b2.BackColor = Color.FromArgb(130, 21, 34);
+            b3.BackColor = Color.FromArgb(130, 21, 34);
+
+            b1.ForeColor = Color.White;
+            b2.ForeColor = Color.White;
+            b3.ForeColor = Color.White;
+
+            label1.Text = b1.Text + " Win";
+        }     
+        //restart
+        private void ButtonPartie_Click(object sender, EventArgs e)
+        {
+            XorO = 0;
+            S = false;
+            win = false;
+            label1.Text = "Play Now";
+            foreach (Control c in panel2.Controls)
+            {
+                if (c is Button)
+                {
+                    c.Text = "";
+                    c.BackColor = Color.White;                     
+                }
+            }
+            SoundPlayer s = new SoundPlayer(@Tic_Tac_Toe.Properties.Resources.Open);
+            s.Play();
+        }  
+    }
+}
